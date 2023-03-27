4fbca671f4615e4136127e8ab8e70f9ea0170b6d
Harold-Morgan <volik46@gmail.com>
2023-03-27 19:44:30 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using Haiku.Bot;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
